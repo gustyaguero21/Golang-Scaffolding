5871856942d0b08460ff2611cddcbde6d310eac0
Gustavo Aguero <gustyaguero21@@gmail.com>
2024-10-15 13:26:52 -0300

    Deleting functions

--- script.py
+++ script.py
@@ -21,26 +21,3 @@ def initialize_project():
 
     return project_path, project_name
 
-def create_file(project_name, file_name, file_content):
-    
-    file_path = Path(file_name)
-
-    with file_path.open("w") as write_file:
-        write_file.write(file_content + " " + project_name.lower())
-
-    if file_name =="go.mod":
-        os.system("go mod tidy")
-
-def create_dir(dir_name):
-    # Crea el directorio en el directorio de trabajo actual
-    directories = Path(dir_name)
-    directories.mkdir(parents=True, exist_ok=True)
-
-# Inicializar el proyecto
-project_path, project_name = initialize_project()
-
-# Crear un archivo
-create_file(project_name, "go.mod", "module")
-
-# Crear un directorio
-create_dir("cmd/api")
