5aabb91ba7490be09d3eafbde83f745230d75da8
Gustavo Aguero <gustyaguero21@@gmail.com>
2024-10-15 21:51:37 -0300

    Commit: Adding configs

--- script.py
+++ script.py
@@ -16,125 +16,66 @@ def initialize_project():
 
     os.chdir(project_path)
 
-    return project_path,stripped_name
+    return project_path, stripped_name
 
-def create_file_in_dir(file_name,file_content,file_destination=None):
-    
-    if file_destination == None:
-        file_path=Path(file_name)
-    
-        with file_path.open("w") as write_file:
-            write_file.write(file_content)
-
-        if file_name == "go.mod":
-            os.system("go mod tidy")
+def create_file_in_dir(file_name, file_content, file_destination=None):
+    if file_destination is None:
+        file_path = Path(file_name)
     else:
-    
         directories = Path(file_destination)
         directories.mkdir(parents=True, exist_ok=True)
-
-        file_path=Path(directories/file_name)
+        file_path = Path(directories / file_name)
     
-        with file_path.open("w") as write_file:
-            write_file.write(file_content)
+    with file_path.open("w") as write_file:
+        write_file.write(file_content)
 
-def create_dirs(directories_str,destination=None):
+    if file_name == "go.mod":
+        os.system("go mod tidy")
+
+def create_dirs(directories_str, destination=None):
     directories = directories_str.split(',')
 
-    if destination =="internal":
-        internal_path=Path("internal")
+    if destination == "internal":
+        internal_path = Path("internal")
         for dir in directories:
-
-            new_dir = Path(internal_path/dir.strip()) 
+            new_dir = Path(internal_path / dir.strip()) 
             new_dir.mkdir(parents=True, exist_ok=True)
-
-            create_file_in_dir(str(dir+".go"),f"package {dir}",new_dir)
-    elif destination ==None:
+            create_file_in_dir(str(dir + ".go"), f"package {dir}", new_dir)
+    elif destination is None:
         for dir in directories:
-
             new_dir = Path(dir.strip()) 
             new_dir.mkdir(parents=True, exist_ok=True)
-
-            create_file_in_dir(str(dir+".go"),f"package {dir}",new_dir)
+            create_file_in_dir(str(dir + ".go"), f"package {dir}", new_dir)
 
 def edit_files(file_path, content):
     # Escribe o sobrescribe el contenido en el archivo dado
     with open(file_path, 'w') as file:
         file.write(content)
-    
 
+def read_code_from_file(file_path):
+    # Aquí se utiliza la ruta relativa correcta
+    absolute_path = Path(__file__).parent / file_path  # Construye la ruta relativa
+    with open(absolute_path, "r") as file:
+        return str(file.read())
 
-project_path,project_name=initialize_project()
+# Inicializa el proyecto y obtén la ruta
+project_path, project_name = initialize_project()
 
-main_code="""
-package main
+# Cargar el contenido de los archivos de la carpeta "golang_codes"
+main_code = read_code_from_file("golang_codes/main.txt")
+config_code = read_code_from_file("golang_codes/config.txt")
+router_code = read_code_from_file("golang_codes/router.txt")
+urlmapping_code = read_code_from_file("golang_codes/urlmapping.txt")
 
-import (
-	"newapp/cmd/config"
-	"newapp/internal/router"
-)
+# Crear los archivos correspondientes en las rutas especificadas
+create_file_in_dir("main.go", main_code, "cmd/api")
+create_file_in_dir("config.go", config_code, "cmd/config")
+create_dirs("data,handlers,mock,models,router,services,utils", "internal")
 
-func main() {
-
-	router := router.SetupRouter()
-
-	router.Run(config.Port)
-}
-"""
-
-create_file_in_dir("main.go",main_code,"cmd/api")
-
-config_code="""
-package config
-
-//router params
-
-const (
-	Port = ":8080"
-)
-
-"""
-
-create_file_in_dir("config.go",config_code,"cmd/config")
-
-create_dirs("data,handlers,mock,models,router,services,utils","internal")
-
-router_code = """
-package router
-
-import "github.com/gin-gonic/gin"
-
-func SetupRouter() *gin.Engine {
-    router := gin.Default()
-
-    Urlmapping(router)
-
-    return router
-}
-"""
-
-urlmapping_code= """
-package router
-
-import "github.com/gin-gonic/gin"
-
-func Urlmapping(router *gin.Engine) {
-    api := router.Group("/api/v1")
-    api.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-}
-"""
-
-
+# Editar archivos específicos
 edit_files("internal/router/router.go", router_code)
-create_file_in_dir("urlmapping.go",urlmapping_code,"internal/router")
+create_file_in_dir("urlmapping.go", urlmapping_code, "internal/router")
 
-
+# Crear el archivo go.mod
 create_dirs("pkg")
-
-create_file_in_dir("go.mod",f"module {project_name.lower()}")
-
-
+create_file_in_dir("go.mod", f"module {project_name.lower()}")
